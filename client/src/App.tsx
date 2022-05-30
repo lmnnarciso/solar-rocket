f31eb16e5b46005585557511f69c99e0fa68101e
lmnnarciso <lmnnarciso14@gmail.com>
2022-05-31 06:54:19 +0800

    Add color theming

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,3 +1,4 @@
+import { createContext, useContext, useState } from "react";
 import { Routes, Route } from "react-router-dom";
 import { Home as HomePage } from "./pages/Home";
 import { Missions as MissionsPage } from "./pages/Missions";
@@ -5,20 +6,76 @@ import { Weather as WeatherPage } from "./pages/Weather";
 import { Preferences as PreferencesPage } from "./pages/Preferences";
 import { _404 } from "./pages/_404";
 import { HelmetProvider, Helmet } from "react-helmet-async";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import { Theme } from "@mui/system";
+
+export const ThemeContext = createContext<any>(undefined);
+
+export const ThemeContextProvider = (props: any) => {
+  const [theme, setTheme] = useState<Theme>(
+    createTheme({
+      palette: {
+        mode: "light",
+      },
+    })
+  );
+
+  const handleUpdateTheme = (newTheme: any) => {
+    setTheme(
+      createTheme({
+        palette: {
+          mode: "light",
+          primary: newTheme,
+        },
+      })
+    );
+  };
+
+  const darkMode = () => {
+    setTheme(
+      createTheme({
+        palette: {
+          mode: "dark",
+        },
+      })
+    );
+  };
+
+  const reset = () => {
+    setTheme(
+      createTheme({
+        palette: {
+          mode: "light",
+        },
+      })
+    );
+  };
+
+  return (
+    <ThemeContext.Provider
+      value={{ theme, handleUpdateTheme, reset, darkMode }}
+    >
+      <ThemeProvider theme={theme}>{props.children}</ThemeProvider>
+    </ThemeContext.Provider>
+  );
+};
 
 function App() {
+  // const ThemeContext = useContext(context);
   return (
     <HelmetProvider>
       <Helmet>
         <title>{process.env.REACT_APP_TITLE}</title>
       </Helmet>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/missions" element={<MissionsPage />} />
-        <Route path="/weather" element={<WeatherPage />} />
-        <Route path="/Preferences" element={<PreferencesPage />} />
-        <Route path="*" element={<_404 />} />
-      </Routes>
+      <ThemeContextProvider>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/missions" element={<MissionsPage />} />
+          <Route path="/weather" element={<WeatherPage />} />
+          <Route path="/Preferences" element={<PreferencesPage />} />
+          <Route path="*" element={<_404 />} />
+        </Routes>
+      </ThemeContextProvider>
     </HelmetProvider>
   );
 }
--- client/src/pages/Preferences.tsx
+++ client/src/pages/Preferences.tsx
@@ -1,11 +1,84 @@
-import { Container } from "@mui/material";
+import { Button, Container, Grid } from "@mui/material";
 import { AppLayout } from "../layouts/AppLayout";
 
+import {
+  lightBlue,
+  blue,
+  red,
+  pink,
+  purple,
+  deepPurple,
+  indigo,
+  cyan,
+  teal,
+  green,
+  lightGreen,
+  lime,
+  yellow,
+  amber,
+  orange,
+  deepOrange,
+  brown,
+  grey,
+  blueGrey,
+} from "@mui/material/colors";
+import { ThemeContext } from "../App";
+import { useContext } from "react";
+
 const Preferences = (): JSX.Element => {
+  const { handleUpdateTheme, reset, darkMode } = useContext<any>(ThemeContext);
+  const themeList = [
+    [lightBlue, "Light Blue"],
+    [blue, "Blue"],
+    [red, "Red"],
+    [pink, "Pink"],
+    [purple, "Purple"],
+    [deepPurple, "Deep Purple"],
+    [indigo, "Indigo"],
+    [cyan, "Cyan"],
+    [teal, "Teal"],
+    [green, "Green"],
+    [lightGreen, "Light Green"],
+    [lime, "Lime"],
+    [yellow, "Yellow"],
+    [amber, "Amber"],
+    [orange, "Orange"],
+    [deepOrange, "Deep Orange"],
+    [brown, "Brown"],
+    [grey, "Grey"],
+    [blueGrey, "Blue Grey"],
+  ];
   return (
     <AppLayout title="Preferences">
       <Container maxWidth="lg">
         <div>Preferences!</div>
+        <Grid container gap={4}>
+          {themeList.map((colorTheme: any) => (
+            <Grid item gap={2}>
+              <Button
+                variant="contained"
+                sx={{
+                  // color: colorTheme[0][500],
+                  bgcolor: colorTheme[0][500],
+                }}
+                onClick={() => handleUpdateTheme(colorTheme[0])}
+              >
+                {/* {colorTheme[1]} */}
+                {colorTheme[1]}
+              </Button>
+            </Grid>
+          ))}
+          <Button variant="contained" onClick={() => reset()}>
+            Reset
+          </Button>
+          <Button
+            variant="contained"
+            sx={{ bgcolor: "black", color: "white" }}
+            onClick={() => darkMode()}
+          >
+            Dark mode
+          </Button>
+        </Grid>
       </Container>
     </AppLayout>
   );
